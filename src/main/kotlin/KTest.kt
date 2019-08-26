db1bbb96e34802b976f64d5a397e799848ae7087
Shamil Gumirov <shamil@gumirov.com>
2019-08-26 09:52:09 +0200

    WIP: External (de)serializer approach

--- src/main/kotlin/KTest.kt
+++ src/main/kotlin/KTest.kt
@@ -4,21 +4,22 @@ import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 
 fun main(args: Array<String>){
-//    serializeDeserializeTest()
+    serializeDeserializeTest()
     deserializeUnknownTypeTest()
 }
 
 fun serializeDeserializeTest() {
     val l = listOf(TR("for_rec", "from_friend", "thankz"),
             TE("for_response", "another_friend", "thankz for repsonse1"))
-    val serializer = GsonBuilder()
+    val gson = GsonBuilder()
             .registerTypeAdapter(M::class.java, MDeserializer())
+            .registerTypeAdapter(M::class.java, MSerializer())
             .excludeFieldsWithoutExposeAnnotation()
             .setPrettyPrinting().create()
-    val s = serializer.toJson(l)
+    val s = gson.toJson(l)
     println("serialize list: " + s)
     val listType = object : TypeToken<List<M>>() { }.type
-    serializer.fromJson<List<M>>(s, listType).forEach{ println("deserialize: $it") }
+    gson.fromJson<List<M>>(s, listType).forEach{ println("deserialize: $it") }
 }
 
 fun deserializeUnknownTypeTest(){
@@ -27,15 +28,23 @@ fun deserializeUnknownTypeTest(){
             .excludeFieldsWithoutExposeAnnotation()
             .setPrettyPrinting()
             .registerTypeAdapter(M::class.java, MDeserializer())
+            .registerTypeAdapter(M::class.java, MSerializer())
             .setPrettyPrinting()
             .create()
     val unknownTypeJson = """[
       {
-        "fw": "for_response",
         "headers":[
           {
             "name": "type",
             "value": "TUnknown"
+          },
+          {
+            "name": "fw",
+            "value": "for_response"
+          },
+          {
+            "name": "link",
+            "value": "a8724sd34r"
           }
         ],
         "f": "another_friend",
@@ -43,33 +52,43 @@ fun deserializeUnknownTypeTest(){
       }
     ]"""
     println("deserialize unknown type: ")
-    gson.fromJson<List<M>>(unknownTypeJson, listType).forEach{
-        println(it)
+    gson.fromJson<List<M>>(unknownTypeJson, listType).forEach{m ->
+        println(m)
         println("-> serialize: ")
-        println(gson.toJson(it))
+        println(gson.toJson(m))
     }
 }
 
 class MDeserializer: JsonDeserializer<M> {
-    companion object {
-        val whitelist = hashSetOf("TR", "TE")
-    }
-
     override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): M {
-        val jobj = json!!.asJsonObject
-        val klass = getHeaderValue(jobj, M.TYPE) ?: M::class.java.name
-        if (!whitelist.contains(klass)) {
-            return Gson().fromJson(jobj, M::class.java).also { m -> m.setHeaderValue(M.TYPE, klass ?: M::class.java.name) }
+        val j = json!!.asJsonObject
+        val a = j.getAsJsonArray("headers")
+        val map: Map<String, String> = a.associate {
+            Pair(it.asJsonObject.get("name")?.asString ?: "", it.asJsonObject.get("value")?.asString ?: "")
         }
-        return context!!.deserialize(jobj, Class.forName(klass))
+        return M(j.get("f").asString, j.get("b").asString, map)
     }
-
-    fun getHeaderValue(jobj: JsonObject, name: String): String? = jobj[M.HEADERS]?.asJsonArray?.find {
+/*    fun getHeaderValue(jobj: JsonObject, name: String): String? = jobj[M.HEADERS]?.asJsonArray?.find {
         it.asJsonObject?.get("name")?.asString.equals(name)
     }?.asJsonObject?.get("value")?.asString
+*/
 }
 
-open class M(@Expose val f: String? = null, @Expose val b: String? = null) {
+class MSerializer: JsonSerializer<M> {
+    override fun serialize(m: M?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+        val o = JsonObject()
+        if (m?.f != null) o.addProperty("f", m.f)
+        if (m?.b != null) o.addProperty("b", m.b)
+        val a = JsonArray()
+        m?.getHeaders()?.forEach { header -> a.add(JsonObject().also{
+            it.addProperty("name", header.name)
+            it.addProperty("value", header.value)
+        }) }
+        return o
+    }
+}
+
+open class M(val f: String? = null, val b: String? = null, protected val deserializedHeaders: Map<String, String>? = null) {
     companion object {
         const val TYPE = "type"
         const val HEADERS = "headers"
@@ -77,25 +96,31 @@ open class M(@Expose val f: String? = null, @Expose val b: String? = null) {
 
     private val type = javaClass.name
 
-    @Expose val headers: MutableList<Header> = mutableListOf(Header(TYPE, type))
+    open fun getHeaders(): List<Header> = listOf(Header(TYPE, type))
 
-    fun setHeaderValue(name: String, value: String) {
-        val header = Header(name, value)
-        with(headers.find { it.name == name }) {
-            this ?: return@with
-            headers.remove(this)
-        }
-        headers.add(header)
-    }
+    //todo replace with Map?
+    data class Header(val name: String, val value: String, val _id: String? = null)
 
     override fun toString(): String {
-        return "$type(f='$f', b='$b', type='$type', headers:\n  $headers\n)"
+        return "$type(f='$f', b='$b', type='$type', headers:\n  " + getHeaders() + "\n)"
     }
-
-    data class Header(val name: String, val value: String, val _id: String? = null)
 }
 
-abstract class T (@Expose val fw: String, f: String, b: String): M(f, b)
+abstract class T (f: String, b: String): M(f, b) {
+    var fw: String? = null
+
+    init {
+        if (deserializedHeaders?.contains("fw") ?: false) fw = deserializedHeaders?.get("fw")
+    }
+    constructor(fw: String, f: String, b: String): this(f, b) {
+        this.fw = fw
+    }
+
+    override fun getHeaders(): List<Header> {
+        val fw = this.fw
+        return if (fw != null) super.getHeaders() + Header("fw", fw) else super.getHeaders()
+    }
+}
 
 class TR(fw: String, f: String, b: String) : T(fw, f, b)
 
